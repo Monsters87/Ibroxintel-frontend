f739dd71d2d75ac19986d44da1fc70b5ab91e699
Monsters87 <134213480+Monsters87@users.noreply.github.com>
2025-06-25 16:54:23 +0100

    Create SummaryStats.jsx

---
+++ src/components/SummaryStats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SummaryStats = () => {
+  return (
+    <div className="summary-stats">
+      <p>Active Rumours: 3</p>
+      <p>Deals Near Confirmation: 1</p>
+    </div>
+  );
+};
+
+export default SummaryStats;
